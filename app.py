9f0c580579018b395db94c86e9dfa8ebc4d245be
I-r-t-a-z-a <syedirtazahasan@outlook.com>
2022-05-21 04:04:14 -0400

    Organized Files + Basic HTML template using bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# app.py
-from flask import Flask           # import flask
-app = Flask(__name__)             # create an app instance
+from flask import Flask
 
-@app.route("/")                   # at the end point /
-def hello():                      # call method hello
-    return "Test Text"         # which returns "hello world"
-if __name__ == "__main__":        # on running python app.py
-    app.run(debug=True)                     # run the flask app
+app = Flask(__name__)
 
-    # app.py: the entry and exit point to our application. application layer
-    # service.py: converst the request into a response. business logic
-    # models.py: handles everything involving database. data
-    # MVC: Model View Controller Pattern
+@app.route('/')
+def index():
+    return "Installation Successful"
+if __name__ == '__main__':
+        app.run()
+            
